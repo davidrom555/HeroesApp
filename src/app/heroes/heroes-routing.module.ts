3b7d4c3ab375c2211cfda9d2d44a38178fdedf45
david <davidrom_desarrollo@hotmai.com>
2024-10-14 14:53:35 -0300

    fix: agrega componente search-heroe

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HerolistComponent } from './pages/hero-list/hero-list.component';
 import { DesignPageComponent } from './pages/design-page/design-page.component';
-// localhost:4200/heroes
+
 const routes: Routes = [
   {
     path: '',
     component: DesignPageComponent,
     children: [
-      { path: 'list', component: HerolistComponent },
+      { path: 'heroes', component: HerolistComponent },
     ]
   }
 ];
---
+++ src/app/heroes/pages/search-heroe/search-heroe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'search-heroe',
+  templateUrl: './search-heroe.component.html',
+  styles: []
+})
+export class SearchPageComponent {
+
+}
