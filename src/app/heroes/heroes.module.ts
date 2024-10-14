ff7e7ca954e9ecd00c76d3d323d0eca29f9322ff
david <davidrom_desarrollo@hotmai.com>
2024-10-14 16:24:50 -0300

    fix: declara componentes de page en HeroesModule

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HerolistComponent } from './pages/hero-list/hero-list.component';
+import { SharedModule } from '../shared/shared.module';
+import { DesignPageComponent } from './pages/design-page/design-page.component';
+import { NewHeroeComponent } from './pages/new-heroe/new-heroe.component';
+import { HeroDetailComponent } from './pages/hero-detail/hero-detail.component';
+import { SearchHeroeComponent } from './pages/search-heroe/search-heroe.component';
 
 @NgModule({
-  declarations: [HerolistComponent],
+  declarations: [
+    HerolistComponent,
+    DesignPageComponent,
+    NewHeroeComponent,
+    HeroDetailComponent,
+    SearchHeroeComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     HeroesRoutingModule,
+    SharedModule
   ]
 })
 export class HeroesModule { }
