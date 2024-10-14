825c0061a68d7e29cae21d5924babd7c4dbd0e38
david <davidrom_desarrollo@hotmai.com>
2024-10-14 16:35:50 -0300

    fix: maquetea estructura sidenav y agrega las opciones label

--- src/app/heroes/pages/design-page/design-page.component.ts
+++ src/app/heroes/pages/design-page/design-page.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'design-page',
   templateUrl: './design-page.component.html',
-  styles: []
+  styleUrls: ['./design-page.component.scss']
 })
 export class DesignPageComponent {
 
+  public sidebarOptions = [
+    { label: 'Heroes', icon: 'label', url: './list' },
+    { label: 'Nuevo Héroe', icon: 'add', url: './new-hero' },
+    { label: 'Buscar Heroe', icon: 'search', url: './search' },
+  ]
 
 }
