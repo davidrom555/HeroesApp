74479742b99d9d90932f4d626658df1a0e0ee651
david <davidrom_desarrollo@hotmai.com>
2024-10-14 15:09:12 -0300

    fix: agrega componente hero-detail

---
+++ src/app/heroes/pages/hero-detail/hero-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styles: [
+  ]
+})
+export class HeroPageComponent implements OnInit {
+
+
+  constructor(
+  ) {}
+
+  ngOnInit(): void {
+    
+  }
+
+}
