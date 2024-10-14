ba2622f85d7b872f80988f744c8582bc9bba12d7
david <davidrom_desarrollo@hotmai.com>
2024-10-14 16:29:01 -0300

    fix: renombra componente HeroePageComponent

--- src/app/heroes/pages/hero-detail/hero-detail.component.ts
+++ src/app/heroes/pages/hero-detail/hero-detail.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class HeroPageComponent implements OnInit {
+export class HeroDetailComponent implements OnInit {
 
 
   constructor(
