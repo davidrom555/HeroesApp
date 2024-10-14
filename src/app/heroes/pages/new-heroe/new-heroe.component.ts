47f1a4df2ceb8579a876dae02617c029b9fb0385
david <davidrom_desarrollo@hotmai.com>
2024-10-14 15:01:04 -0300

    fix: agrega nuevo componente new-heroe

---
+++ src/app/heroes/pages/new-heroe/new-heroe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'new-heroe',
+  templateUrl: './new-heroe.component.html',
+  styles: []
+})
+export class NewHeroeComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+}
