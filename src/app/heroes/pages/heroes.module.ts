64cf6b51db7452284f065aea8f3c8e1a4bd2bf6c
david <davidrom_desarrollo@hotmai.com>
2024-10-14 12:41:27 -0300

    fix: renombra carpeta hero-list

--- src/app/heroes/pages/heroes.module.ts
+++ src/app/heroes/pages/heroes.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HeroesRoutingModule } from './heroes-routing.module';
-import { HerolistComponent } from './list-page/hero-list.component';
+import { HerolistComponent } from './hero-list/hero-list.component';
 
 @NgModule({
   declarations: [HerolistComponent],
