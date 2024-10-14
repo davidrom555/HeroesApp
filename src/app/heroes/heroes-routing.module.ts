50acfb232ad66642ef7ba24915d56c397e835849
david <davidrom_desarrollo@hotmai.com>
2024-10-14 16:46:54 -0300

    fix: configura el heroes-routing para la navegacion a las diferentes pantallas

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HerolistComponent } from './pages/hero-list/hero-list.component';
 import { DesignPageComponent } from './pages/design-page/design-page.component';
+import { NewHeroeComponent } from './pages/new-heroe/new-heroe.component';
+import { SearchHeroeComponent } from './pages/search-heroe/search-heroe.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DesignPageComponent,
     children: [
-      { path: 'heroes', component: HerolistComponent },
+      { path: 'list', component: HerolistComponent },
+      { path: 'new-hero', component: NewHeroeComponent },
+      { path: 'search', component: SearchHeroeComponent },
+      { path: ':id', component: DesignPageComponent},
+      { path: 'edit/:id', component: NewHeroeComponent },
+      { path: '**', redirectTo: 'list' },
     ]
   }
 ];
