517c76875d14f8d4e88c67151d40ff6102475a72
david <davidrom_desarrollo@hotmai.com>
2024-10-15 19:32:41 -0300

    fix: renombra componente NewHeroeComponent

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HerolistComponent } from './pages/hero-list/hero-list.component';
 import { DesignPageComponent } from './pages/design-page/design-page.component';
-import { NewHeroeComponent } from './pages/new-heroe/new-heroe.component';
 import { SearchHeroeComponent } from './pages/search-heroe/search-heroe.component';
+import { NewHeroComponent } from './pages/new-hero/new-hero.component';
 
 const routes: Routes = [
   {
@@ -11,10 +11,10 @@ const routes: Routes = [
     component: DesignPageComponent,
     children: [
       { path: 'list', component: HerolistComponent },
-      { path: 'new-hero', component: NewHeroeComponent },
+      { path: 'new-hero', component: NewHeroComponent },
       { path: 'search-hero', component: SearchHeroeComponent },
       { path: ':id', component: DesignPageComponent},
-      { path: 'edit/:id', component: NewHeroeComponent },
+      { path: 'edit/:id', component: NewHeroComponent },
       { path: '**', redirectTo: 'list' },
     ]
   }
