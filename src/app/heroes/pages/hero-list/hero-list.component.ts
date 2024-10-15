86842f1f12c3f5d50fe711b7f591d261fab5e377
david <davidrom_desarrollo@hotmai.com>
2024-10-15 20:24:53 -0300

    fix: se llama al service getHeroes desde el componente heroList

--- src/app/heroes/pages/hero-list/hero-list.component.ts
+++ src/app/heroes/pages/hero-list/hero-list.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Hero } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes-service.service';
-
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'hero-list',
   templateUrl: './hero-list.component.html',
   styles: []
 })
-export class HerolistComponent implements OnInit {
+export class HerolistComponent implements OnInit, OnDestroy {
 
   public heroes: Hero[] = [];
+  private heroesSubscription!: Subscription;
 
-  constructor( private heroesService: HeroesService ) {}
+  constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
-   
+    this.heroesSubscription = this.heroesService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
+  ngOnDestroy(): void {
+    // Asegurarse de desuscribirse para evitar fugas de memoria
+    this.heroesSubscription.unsubscribe();
+  }
 }
