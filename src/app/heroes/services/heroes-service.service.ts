d64c7cb9e2f4ba99ae97633aef3c59be56221cc3
david <davidrom_desarrollo@hotmai.com>
2024-10-15 20:23:01 -0300

    fix: agrega los diferentes servicios que se van a utilizar usando localStorage

--- src/app/heroes/services/heroes-service.service.ts
+++ src/app/heroes/services/heroes-service.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { Hero } from '../interfaces/hero.interface';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroesService {
 
-  constructor() { }
+  private localStorageKey = 'heroes';
+  private heroesSubject: BehaviorSubject<Hero[]> = new BehaviorSubject<Hero[]>(this.loadFromLocalStorage());
+
+  constructor() {}
+
+  getHeroes(): Observable<Hero[]> {
+    return this.heroesSubject.asObservable();
+  }
+
+  // servicio para agregar héroes y actualizar el localStorage
+  addHero(hero: Hero): void {
+    const currentHeroes = this.heroesSubject.getValue();
+    const updatedHeroes = [...currentHeroes, hero];
+    this.updateHeroes(updatedHeroes);
+  }
+
+  // servicio para eliminar un héroe por ID
+  removeHero(id: string): void {
+    const updatedHeroes = this.heroesSubject.getValue().filter(hero => hero.id !== id);
+    this.updateHeroes(updatedHeroes);
+  }
+
+  // Actualiza tanto el localStorage como el BehaviorSubject
+  private updateHeroes(heroes: Hero[]): void {
+    this.heroesSubject.next(heroes);
+    this.saveToLocalStorage(heroes);
+  }
+
+  // Cargar héroes desde el localStorage
+  private loadFromLocalStorage(): Hero[] {
+    const heroes = localStorage.getItem(this.localStorageKey);
+    return heroes ? JSON.parse(heroes) : [];
+  }
+
+  private saveToLocalStorage(heroes: Hero[]): void {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(heroes));
+  }
 }
