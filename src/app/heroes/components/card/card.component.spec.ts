65a98de48b9234a4d4041197d31307498315f260
david <davidrom_desarrollo@hotmai.com>
2024-10-22 18:11:47 -0300

    fix: modifica archivo spect de la card para las pruebas unitarias

--- src/app/heroes/components/card/card.component.spec.ts
+++ src/app/heroes/components/card/card.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CardComponent } from './card.component';
-
+import { Hero } from '../../interfaces/hero.interface';
 describe('CardComponent', () => {
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
@@ -11,10 +11,29 @@ describe('CardComponent', () => {
     });
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    // Agregar un héroe para las pruebas
+    component.hero = {
+      id: '1',
+      name: 'Superman',
+      power: 'Super Strength',
+      image: null, 
+      alt_img: 'superman-alt-image.png'
+    } as Hero;
+
+    fixture.detectChanges(); // Detectar cambios en el componente
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit onDelete event when onDeleteHero is called', () => {
+    spyOn(component.onDelete, 'emit'); // Espiar el método emit
+
+    const heroId = component.hero.id;
+    component.onDeleteHero(heroId);
+
+    expect(component.onDelete.emit).toHaveBeenCalledWith(heroId); // Verificar que se emitió el evento con el ID correcto
   });
 });
