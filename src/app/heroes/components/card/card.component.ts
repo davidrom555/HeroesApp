51daa4e511f43932d702f5b5cbbcd8c2590de0f7
david <davidrom_desarrollo@hotmai.com>
2024-10-22 17:55:41 -0300

    fix: elimina loading del componente card

--- src/app/heroes/components/card/card.component.ts
+++ src/app/heroes/components/card/card.component.ts
@@ -12,9 +12,8 @@ export class CardComponent implements OnInit {
   @Input()
   public hero!: Hero;
   @Output() onDelete = new EventEmitter<string | null>(); // Emitirá el término de búsqueda
-  public heroesSignal = signal<Hero[]>([]);
-  public loadingSignal = this.loadingService.loading$;
-  constructor(private loadingService:LoadingService) {}
+ 
+  constructor() {}
 
   ngOnInit(): void {}
 
